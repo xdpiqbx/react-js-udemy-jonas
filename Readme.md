c0e802d3a63a851cb2edb52b81197ace1bc67a0e
xdpiqbx <xdpiqbx@gmail.com>
2023-10-01 11:41:43 +0300

    in process

---
+++ Readme.md
@@ -0,0 +1,10 @@
+VS Code
+format on save
+[ Ctrl + , ] - Settings
+[ Ctrl + Shift + ` ] - Terminal
+
+Create React App
+`npm create vite@latest`
+`npm i`
+
+npm install bootstrap
--- section-006-state-events-forms-interactiv-components/002-far-away/src/App.js
+++ section-006-state-events-forms-interactiv-components/002-far-away/src/App.js
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -16,17 +25,63 @@ function Logo() {
 }
 
 function Form() {
+
+    const [description, setDescription] = useState("")
+    const [quantity, setQuantity] = useState(1)
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (!description) {
+            return
+        }
+        const newItemObject = {
+            id: initialItems[initialItems.length - 1].id + 1,
+            description: description,
+            quantity: quantity,
+            packed: false
+        }
+        setDescription("")
+        setQuantity(1)
+        console.log(newItemObject);
+        // initialItems.push(newItemObject)
+    }
     return (
-      <div className="add-form">
-        <h3>What do you need for your 🤩 trip?</h3>  
-      </div>
+      <form className="add-form" onSubmit={handleSubmit}>
+            <label>What do you need for your 🤩 trip?</label>
+            <select value={quantity} onChange={(event)=>setQuantity(Number(event.target.value))}>
+                {
+                    Array.from({ length: 15 }, (_, i) => i + 1).map(
+                        i => <option name="quantity" key={i}>{i}</option>
+                    )
+                }
+            </select>
+            <input
+                type='text' placeholder="Item..."
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+            />
+            <button>Add</button>
+      </form>
     )
 }
 
 function PackingList() {
     return (
-        <div className="list">PackingList</div>
+        <div className="list">
+            <ul>
+                {initialItems.map(item => <Item item={ item } key={ item.id }/>)}
+            </ul>
+        </div>
     )
+}
+
+function Item({ item }) {
+    return <li>
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+            {item.quantity} {item.description}
+        </span>
+        <button>❌</button>
+    </li>
 }
 
 function Stats() {
