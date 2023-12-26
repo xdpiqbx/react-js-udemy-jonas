946b04c732174a4e9d5559cfc5c70e4442894d0d
xdpiqbx <xdpiqbx@gmail.com>
2023-12-26 07:56:52 +0200

    start with 97

--- section-008-practice-eat_and_split/src/App.js
+++ section-008-practice-eat_and_split/src/App.js
@@ -22,77 +22,128 @@ const initialFriends = [
 export default function App() {
   return (
     <div className="app">
-      <Friends />
-      <SplitBill />
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
-function AddFriend() {
+function FriendsList() {
+  const friends = initialFriends;
   return (
-    <form className="form-add-friend">
-      <label htmlFor="text">👫 Friend name</label>
-      <input id="text" type="text" />
-      <label htmlFor="img">🌄 Image URL</label>
-      <input id="img" type="text" />
-      <Button>Add</Button>
-    </form>
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
   );
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
-}
-
-function Friend({ id, image, balance, name }) {
+function Friend({ friend }) {
   return (
-    <li className="selected">
-      <img alt={name} src={image}></img>
-      <h3>{name}</h3>
-      <p>You and {name} are even</p>
-      <p className="green">
-        {name} owes you {balance}€
-      </p>
-      <p className="red">
-        You owe {name} {balance}€{" "}
-      </p>
-      <Button>"Close" : "Select"</Button>
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
     </li>
   );
 }
 
-export function Friends() {
-  return (
-    <div className="sidebar">
-      <ul>
-        {initialFriends.map(({ id, image, balance, name }) => (
-          <Friend key={id} image={image} balance={balance} name={name} />
-        ))}
-      </ul>
-      <AddFriend />
-      <Button>Close</Button>
-      <Button>Add friend</Button>
-    </div>
-  );
-}
+// export default function App() {
+//   return (
+//     <div className="app">
+//       <Friends />
+//       <SplitBill />
+//     </div>
+//   );
+// }
 
-export function SplitBill() {
-  return (
-    <form className="form-split-bill">
-      <h2>SPLIT A BILL WITH NAME</h2>
-      <label htmlFor="bill">💰 Bill value</label>
-      <input id="bill" type="text" />
-      <label htmlFor="">🧍‍♀️ Your expense</label>
-      <input type="text" />
-      <label>👫 friend.name &apos;s expense</label>
-      <input type="text" disabled />
-      <label htmlFor="">🤑 Who is paying the bill</label>
-      <select>
-        <option value="you">You</option>
-        <option value="other">friend.name</option>
-      </select>
+// export default function App() {
+//   return (
+//     <div className="app">
+//       <Friends />
+//       <SplitBill />
+//     </div>
+//   );
+// }
 
-      <Button>Split bill</Button>
-    </form>
-  );
-}
+// function AddFriend() {
+//   return (
+//     <form className="form-add-friend">
+//       <label htmlFor="text">👫 Friend name</label>
+//       <input id="text" type="text" />
+//       <label htmlFor="img">🌄 Image URL</label>
+//       <input id="img" type="text" />
+//       <Button>Add</Button>
+//     </form>
+//   );
+// }
+
+// function Button({ children }) {
+//   return <button className="button">{children}</button>;
+// }
+
+// function Friend({ id, image, balance, name }) {
+//   return (
+//     <li className="selected">
+//       <img alt={name} src={image}></img>
+//       <h3>{name}</h3>
+//       <p>You and {name} are even</p>
+//       <p className="green">
+//         {name} owes you {balance}€
+//       </p>
+//       <p className="red">
+//         You owe {name} {balance}€{" "}
+//       </p>
+//       <Button>"Close" : "Select"</Button>
+//     </li>
+//   );
+// }
+
+// export function Friends() {
+//   return (
+//     <div className="sidebar">
+//       <ul>
+//         {initialFriends.map(({ id, image, balance, name }) => (
+//           <Friend key={id} image={image} balance={balance} name={name} />
+//         ))}
+//       </ul>
+//       <AddFriend />
+//       <Button>Close</Button>
+//       <Button>Add friend</Button>
+//     </div>
+//   );
+// }
+
+// export function SplitBill() {
+//   return (
+//     <form className="form-split-bill">
+//       <h2>SPLIT A BILL WITH NAME</h2>
+//       <label htmlFor="bill">💰 Bill value</label>
+//       <input id="bill" type="text" />
+//       <label htmlFor="">🧍‍♀️ Your expense</label>
+//       <input type="text" />
+//       <label>👫 friend.name &apos;s expense</label>
+//       <input type="text" disabled />
+//       <label htmlFor="">🤑 Who is paying the bill</label>
+//       <select>
+//         <option value="you">You</option>
+//         <option value="other">friend.name</option>
+//       </select>
+
+//       <Button>Split bill</Button>
+//     </form>
+//   );
+// }
